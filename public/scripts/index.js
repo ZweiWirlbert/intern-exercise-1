63b89c4c5be166155e9e6c0fc037f6039dfc82b4
Vinh Thuy <rickhoang42@gmail.com>
2022-04-03 16:57:52 +0700

    update: complete adding link into buttons

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -21,3 +21,42 @@ function openMenu() {
   var menu = document.getElementById("overlay-menu");
   menu.classList.toggle("menu-open");
 }
+
+const modal = document.getElementById("popupBox");
+
+const openBtn = document.getElementById("openBtn");
+
+const closeBtn = document.getElementsByClassName("popup-close-btn")[0];
+
+const wrapper = document.getElementsByClassName("modal-wrapper")[0];
+
+openBtn.onclick = function () {
+  modal.style.display = "block";
+  wrapper.style.display = "block";
+
+  wrapper.style.opacity = "1";
+};
+
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+  wrapper.style.display = "none";
+
+  wrapper.style.opacity = "0";
+};
+
+wrapper.onclick = function () {
+  modal.style.display = "none";
+  wrapper.style.display = "none";
+
+  wrapper.style.opacity = "0";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+modal.onclick = function (e) {
+  e.stopPropagation();
+};
