fa24d978f5dd41d91fe7c87526d1bc5ce08591b6
Vinh Thuy <rickhoang42@gmail.com>
2022-04-05 14:22:56 +0700

    update: fix problem and adjust link for some pages

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -13,50 +13,5 @@ function openCity(evt, tabName) {
   }
 
   document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
   evt.currentTarget.style.borderColor = "#70c174";
 }
-
-function openMenu() {
-  var menu = document.getElementById("overlay-menu");
-  menu.classList.toggle("menu-open");
-}
-
-const modal = document.getElementById("popupBox");
-
-const openBtn = document.getElementById("openBtn");
-
-const closeBtn = document.getElementsByClassName("popup-close-btn")[0];
-
-const wrapper = document.getElementsByClassName("modal-wrapper")[0];
-
-openBtn.onclick = function () {
-  modal.style.display = "block";
-  wrapper.style.display = "block";
-
-  wrapper.style.opacity = "1";
-};
-
-closeBtn.onclick = function () {
-  modal.style.display = "none";
-  wrapper.style.display = "none";
-
-  wrapper.style.opacity = "0";
-};
-
-wrapper.onclick = function () {
-  modal.style.display = "none";
-  wrapper.style.display = "none";
-
-  wrapper.style.opacity = "0";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-modal.onclick = function (e) {
-  e.stopPropagation();
-};
